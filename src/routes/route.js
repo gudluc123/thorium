45af3196583abab6720351f414c7724f7ae26865
gudluc123 <rahulkumarkiit94@gmail.com>
2022-02-22 23:10:10 +0530

    2ndassignmentcommit

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+router.get('/movies', function(req, res) {
+   
+    res.send('["fukrey","rang de basanti","one","avengers"]')
 })
+
+router.get('/movies/:movieid',function(req,res){
+      
+let movi=["fukrey","one","two","three","four"]
+let value=req.params.movieid;
+if(value>movi.length-1){
+    res.send('does not exists')
+}else{
+    res.send(movi[value])
+}
+
+})
+router.get('/movies',function(req,res){
+
+    res.send('[{id:1,name:"harry poter1"},{id:2,name:"harry poter3"},{id:3,name:"harry potter4"}]')
+})
+
+router.get('/cinema/:cinemaid',function(req,res){
+   let cinem=[{id:1,name:"harry poter1"},{id:2,name:"harry poter3"},{id:3,name:"harry potter4"}]
+   let value =req.params.cinemaid;
+   let found =false;
+   for(let i=0;i<cinem.length;i++){
+if(cinem[i].id==value){
+    found=true;
+    res.send(cinem[i])
+    break;
+}
+   }
+})
+
 
 
 
