432d3d21b35d8cdf13df0cb01926a2a1dfdcefe5
gudluc123 <rahulkumarkiit94@gmail.com>
2022-03-08 23:50:16 +0530

    assignment complete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,14 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-const createUser = async function (abcd, xyz) {
+const createUser = async function (req, res) {
   //You can name the req, res objects anything.
   //but the first parameter is always the request 
   //the second parameter is always the response
-  let data = abcd.body;
+  let data = req.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
+  // console.log(abcd.newAtribute);
+  res.send({ msg: savedData });
 };
 
 const loginUser = async function (req, res) {
@@ -84,7 +84,24 @@ const updateUser = async function (req, res) {
   res.send({ status: updatedUser, data: updatedUser });
 };
 
+const deleteUser = async function(req,res){
+
+  let token = req.headers["x-auth-token"]
+  if (!token)
+  return res.send({status:false , msg:"token is not present"})
+  
+  let decodedToken =jwt.verify(token,"functionup-thorium")
+
+    if (!decodedToken)
+    return res.send({status:false, msg:"token is invalid"})
+    
+   let userId =req.params.userId
+   let deletedUser = await userModel.updateMany({_id:userId},{$set:{isDeleted:true}})
+   res.send('data is deleted')
+}
+
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteUser=deleteUser
